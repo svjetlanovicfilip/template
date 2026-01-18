a3d64d1047fdae2a940babe168415bb5a8c2c95f
Filip Svjetlanovic <filip.svjetlanovic@levi9.com>
2026-01-18 14:54:44 +0100

    change routing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calendar_view/calendar_view.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -7,10 +8,12 @@ import 'common/constants/routes.dart';
 import 'common/di/di_container.dart';
 import 'common/widgets/screens/splash_screen.dart';
 import 'config/style/theme.dart';
+import 'features/calendar/data/models/slot.dart';
 import 'features/calendar/ui/screens/book_appointment_screen.dart';
 import 'features/calendar/ui/screens/home_screen.dart';
 import 'features/login/ui/screens/forgot_password_screen.dart';
 import 'features/login/ui/screens/login_screen.dart';
+import 'features/service/data/models/service_type.dart';
 import 'features/service/ui/screens/add_edit_service_screen.dart';
 import 'features/service/ui/screens/service_list_screen.dart';
 import 'features/settings/ui/screens/add_edit_employee_screen.dart';
@@ -54,23 +57,72 @@ class _MyAppState extends State<MyApp> {
         theme: lightTheme,
         initialRoute: Routes.splash,
         debugShowCheckedModeBanner: false,
-        routes: {
-          Routes.splash: (context) => const SplashScreen(),
-          Routes.login: (context) => const LoginScreen(),
-          Routes.home: (context) => const ExtractHomeScreenArguments(),
-          Routes.settings: (context) => const SettingsScreen(),
-          Routes.changePasswordScreen:
-              (context) => const ChangePasswordScreen(),
-          Routes.changeTitleScreen: (context) => const ChangeTitleScreen(),
-          Routes.employeesScreen: (context) => const EmployeesScreen(),
-          Routes.addEditmployeesScreen:
-              (context) => const AddEditEmployeeScreen(),
-          Routes.bookAppointment:
-              (context) => const ExtractBookAppointmentArgumentsScreen(),
-          Routes.serviceListScreen: (context) => const ServiceListScreen(),
-          Routes.addEditServicesScreen:
-              (context) => const ExtractAddEditServiceScreenArguments(),
-          Routes.forgotPassword: (context) => const ForgotPasswordScreen(),
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case Routes.splash:
+              return CupertinoPageRoute(
+                builder: (context) => const SplashScreen(),
+              );
+            case Routes.login:
+              return CupertinoPageRoute(
+                builder: (context) => const LoginScreen(),
+              );
+            case Routes.home:
+              return CupertinoPageRoute(
+                builder:
+                    (context) => HomeScreen(
+                      slots: settings.arguments as List<Slot>? ?? [],
+                    ),
+              );
+            case Routes.settings:
+              return CupertinoPageRoute(
+                builder: (context) => const SettingsScreen(),
+              );
+            case Routes.changePasswordScreen:
+              return CupertinoPageRoute(
+                builder: (context) => const ChangePasswordScreen(),
+              );
+            case Routes.changeTitleScreen:
+              return CupertinoPageRoute(
+                builder: (context) => const ChangeTitleScreen(),
+              );
+            case Routes.employeesScreen:
+              return CupertinoPageRoute(
+                builder: (context) => const EmployeesScreen(),
+              );
+            case Routes.addEditmployeesScreen:
+              return CupertinoPageRoute(
+                builder: (context) => const AddEditEmployeeScreen(),
+              );
+            case Routes.bookAppointment:
+              return CupertinoPageRoute(
+                builder:
+                    (context) => BookAppointmentScreen(
+                      slot: settings.arguments as Slot?,
+                    ),
+              );
+            case Routes.serviceListScreen:
+              return CupertinoPageRoute(
+                builder: (context) => const ServiceListScreen(),
+              );
+            case Routes.addEditServicesScreen:
+              return CupertinoPageRoute(
+                builder:
+                    (context) => AddEditServiceScreen(
+                      service: settings.arguments as ServiceType?,
+                    ),
+              );
+            case Routes.forgotPassword:
+              return CupertinoPageRoute(
+                builder: (context) => const ForgotPasswordScreen(),
+              );
+            default:
+              return MaterialPageRoute(
+                builder:
+                    (context) =>
+                        const Scaffold(body: Center(child: Text('404'))),
+              );
+          }
         },
       ),
     );
